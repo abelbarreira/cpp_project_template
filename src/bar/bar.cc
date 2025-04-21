2ba45b993904fc4b973b1c8a4cb196260faf6a89
abelbarreira <abelbarreira@hotmail.com>
2025-04-21 14:34:44 +0200

    chore: unformatted file

--- src/bar/bar.cc
+++ src/bar/bar.cc
@@ -12,10 +12,8 @@
  * @param do_branch2
  * @return int
  */
-int fn_branch(bool do_branch1, bool do_branch2)
-{
-    if (do_branch1 || do_branch2)
-        return 0;
+int fn_branch(bool do_branch1, bool do_branch2)    {
+    if (do_branch1    ||     do_branch2)
+        return 0  ;
 
-    return 1;
-}
+return 1; }
