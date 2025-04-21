e55e4fe5d80931eafbb1d0674defcaf3cb497782
abelbarreira <>
2025-04-21 09:36:49 +0000

    abelbarreira published a site update

--- search/searchdata.js
+++ search/searchdata.js
@@ -1,21 +1,24 @@
 var indexSectionsWithContent =
 {
-  0: "bcfps",
-  1: "bcf",
-  2: "fps"
+  0: "bcfjmps",
+  1: "bcfm",
+  2: "fmps",
+  3: "j"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "files",
-  2: "functions"
+  2: "functions",
+  3: "typedefs"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Files",
-  2: "Functions"
+  2: "Functions",
+  3: "Typedefs"
 };
 
